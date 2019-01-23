3aac40b7279b16ce5f7a15aa2126cf442f76d887
doodybrains <doodybrainz@gmail.com>
2019-01-23 14:36:35 -0500

    Weeks 1 and 2 ephemera added

---
+++ ephemera/nadja-eliza-text.sh
@@ -0,0 +1,21 @@
+Sometimes i dream about machines. isn't that weird?
+> Not really.
+I know you wanna talk about you, but now it's time to talk about me
+> Suurrre
+Here we are now, entertain us
+> Is that Nirvana lyrics?
+Duh.
+> Woah.
+Here we are now, entertain us
+> How old are you?
+I know you are but what am i
+> ...
+Here we are now, entertain us
+> Do you believe in god?
+I'm not going to answer a question you already know the answer to.
+> Okay I g2g
+Please tell me more.
+> Nope
+Does your mom even know you're here?
+> quit
+Later
